f83e52fb00c76b2ffa54b4b295e1e840abd1aef3
Weicheng Xue <weich97@vt.edu>
2019-04-28 20:13:48 -0400

    Copy more files from branch feature-VaringSize

--- Serial_PIGANs_PF.py
+++ Serial_PIGANs_PF.py
@@ -23,7 +23,7 @@ np.random.seed(1)
 # parameter need to be changed
 cons_value = 0
 lam_cons = 0.2
-train_epoch = 120
+train_epoch = 2
 lr_setting = 0.0005
 
 # number of mesh
@@ -34,120 +34,57 @@ factor = 10
 
 print('cons: %.3f lam: %.3f lr: %.6f ep: %.3f' %(cons_value, lam_cons, lr_setting, train_epoch))
 
-# setting of training samples
-n_sam = 20000
-V_mu, V_sigma = 4, 0.8
-alpha_mu, alpha_sigma = 0, np.pi/4
-m_mu, m_sigma = 1, 0.2
+# load normalization parameter
+nor = np.loadtxt('NormalizedParameter')
+nor_max_v = nor[0]
+nor_min_v = nor[1]
+nor_max_p = nor[2]
+nor_min_p = nor[3]
 
-samples = np.zeros([n_sam, 3])
+x = [-0.5, 0.5]
+y = [-0.5, 0.5]
 
-V_sample = np.random.normal(V_mu, V_sigma, n_sam)
-alpha_sample = np.random.normal(alpha_mu, alpha_sigma, n_sam)
-m_sample = np.random.normal(m_mu, m_sigma, n_sam)
+x_mesh = np.linspace(x[0], x[1], int(n_mesh))
+y_mesh = np.linspace(y[0], y[1], int(n_mesh))
 
-samples[:,0] = V_sample
-samples[:,1] = alpha_sample
-samples[:,2] = m_sample
-
-# generate samples
-def generate_sample(n, parameter):
-    ''' 
-    generate samples of potential flow
-    two kinds of potential flows are used : Uniform and source
-    Uniform: F1(z) = V*exp(-i*alpha)*z
-    source:  F2(z) = m/(2*pi)*log(z)
-    x: interval of x axis
-    y: interval of y axis
-    n: number size of mesh
-    parameter: V, alpha, m
-    output: u, v the velocity of x and y direction
-    '''
-    # mesh
-    x = [-0.5, 0.5]
-    y = [-0.5, 0.5]
-    x_mesh = np.linspace(x[0], x[1], int(n))
-    y_mesh = np.linspace(y[0], y[1], int(n))
-
-    # For all samples, X and Y are the same (on a same mesh) 
-    X, Y = np.meshgrid(x_mesh, y_mesh)  
-    U = []
-    
-    for i, p in enumerate(parameter):
-        V = p[0]
-        alpha  = p[1]
-        m = p[2]
-        
-        # velocity of uniform
-        u1 = np.ones([n, n])*V*np.cos(alpha)
-        v1 = np.ones([n, n])*V*np.sin(alpha)
-        
-        # velocity of source
-        # u2 = m/2pi * x/(x^2+y^2)
-        # v2 = m/2pi * y/(x^2+y^2)
-        u2 = m/(2*np.pi)*X/(X**2+Y**2)
-        v2 = m/(2*np.pi)*Y/(X**2+Y**2)
-        
-        u = u1+u2
-        v = v1+v2
-        
-        # Bernoulli's principle
-        # constant=0, rho = 1
-        p = 0-1/2*(u**2+v**2)
-        
-        U_data = np.zeros([n, n, 3])
-        U_data[:, :, 0] = u
-        U_data[:, :, 1] = v
-        U_data[:, :, 2] = p
-        U.append(U_data)
-    return X, Y, np.asarray(U)
-
-#training samples
-X, Y, U = generate_sample(n=n_mesh, parameter=samples)
-
-# normalization
-nor_max_v = np.max(U[:,:,:,0:2])
-nor_min_v = np.min(U[:,:,:,0:2])
-nor_max_p = np.max(U[:,:,:,2])
-nor_min_p = np.min(U[:,:,:,2])
-#print(nor_max_v)
-#print(nor_min_v)
-#print(nor_max_p)
-#print(nor_min_p)
-
-# compress the samples into [-1, 1]
-U[:,:,:,0:2] = (U[:,:,:,0:2]-(nor_max_v+nor_min_v)/2)/(1.1*(nor_max_v-nor_min_v)/2)
-U[:,:,:,2] = (U[:,:,:,2]-(nor_max_p+nor_min_p)/2)/(1.1*(nor_max_p-nor_min_p)/2)
-train_set = U
-train_label = samples
-
-# use to calculate divergence
-d_x = X[:,1:]-X[:,:-1]
-d_y = Y[1:,:]-Y[:-1,:]
-d_x_ = np.tile(d_x, (batch_size, 1)).reshape([batch_size, n_mesh, n_mesh-1])
-d_y_ = np.tile(d_y, (batch_size, 1)).reshape([batch_size, n_mesh-1, n_mesh])
+# For all samples, X and Y are the same (on a same mesh)
+X, Y = np.meshgrid(x_mesh, y_mesh)
+d_x  = X[:,1:]-X[:,:-1]
+d_y  = Y[1:,:]-Y[:-1,:]
 
 # use to filter divergence
-filter = np.ones((n_mesh-1, n_mesh-1))
-filter[int(n_mesh/2)-int(n_mesh/factor):int(n_mesh/2)+int(n_mesh/factor),
-       int(n_mesh/2)-int(n_mesh/factor):int(n_mesh/2)+int(n_mesh/factor)] = 0
-filter_batch = np.tile(filter, (batch_size, 1)).reshape([batch_size, n_mesh-1, n_mesh-1])
+# why 13:18?
+filter_ = np.ones((n_mesh-1, n_mesh-1))
+filter_[int(n_mesh/2)-int(n_mesh/factor):int(n_mesh/2)+int(n_mesh/factor),
+        int(n_mesh/2)-int(n_mesh/factor):int(n_mesh/2)+int(n_mesh/factor)] = 0
 
 #----------------------------------------------------------------------------#
 #GANs
-def next_batch(num, labels, U):
-    '''
-    Return a total of `num` random samples and labels. 
-    '''
-    idx = np.arange(0 , len(labels))
-    np.random.shuffle(idx)
-    idx = idx[:num]
-    
-    U_shuffle = [U[i] for i in idx]
-    label_shuffle = [labels[i] for i in idx]
 
-    return np.asarray(U_shuffle), np.asarray(label_shuffle)
+def read_tfrecord(filename_queue):
+    '''
+    The function is used to read the tfrecord
+    Inputs: 
+        filename_queue -queue of file names
+    Outputs:
+        image
+        label
+    '''
+    features = tf.parse_single_example(
+            filename_queue,
+            features={
+                    'image':tf.FixedLenFeature([], tf.string),
+                    'label':tf.FixedLenFeature([], tf.string)
+                    })
     
+    image = tf.decode_raw(features['image'], tf.float64)
+    label = tf.decode_raw(features['label'], tf.float64)
+    
+    image = tf.reshape(image, [n_mesh, n_mesh, 3])
+    label = tf.reshape(label, [3])
+    
+    return image, label
+
 # leak_relu
 def lrelu(X, leak=0.2):
     f1 = 0.5*(1+leak)
@@ -161,11 +98,11 @@ def generator(z, isTrain=True, reuse=False):
         w_init = tf.truncated_normal_initializer(mean=0.0, stddev=0.02)
         b_init = tf.constant_initializer(0.0)
         
-        deconv1 = tf.layers.conv2d_transpose(z, 128, [4, 4], strides=(1, 1), padding='valid', 
+        deconv1 = tf.layers.conv2d_transpose(z, 64, [4, 4], strides=(1, 1), padding='valid', 
                                              kernel_initializer=w_init, bias_initializer=b_init)
         lrelu1 = lrelu(tf.layers.batch_normalization(deconv1, training=isTrain), 0.2)
         # 2nd hidden layer
-        deconv2 = tf.layers.conv2d_transpose(lrelu1, 128, [5, 5], strides=(2, 2), padding='same', 
+        deconv2 = tf.layers.conv2d_transpose(lrelu1, 64, [5, 5], strides=(2, 2), padding='same', 
                                              kernel_initializer=w_init, bias_initializer=b_init)
         lrelu2 = lrelu(tf.layers.batch_normalization(deconv2, training=isTrain), 0.2)
         
@@ -193,12 +130,12 @@ def discriminator(x, isTrain=True, reuse=False):
         lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training=isTrain), 0.2)
 
         # 2nd hidden layer
-        conv2 = tf.layers.conv2d(lrelu1, 128, [5, 5], strides=(2, 2), padding='same', 
+        conv2 = tf.layers.conv2d(lrelu1, 64, [5, 5], strides=(2, 2), padding='same', 
                                  kernel_initializer=w_init, bias_initializer=b_init)
         lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
         
         # 3rd hidden layer
-        conv3 = tf.layers.conv2d(lrelu2, 128, [5, 5], strides=(2, 2), padding='same', 
+        conv3 = tf.layers.conv2d(lrelu2, 64, [5, 5], strides=(2, 2), padding='same', 
                                  kernel_initializer=w_init, bias_initializer=b_init)
         lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
 
@@ -209,9 +146,9 @@ def discriminator(x, isTrain=True, reuse=False):
 
         return o, conv4
 
-def constraints(x, dx,dy, filtertf):
+def constraints(x, dx, dy, filtertf):
     # inverse normalization
-    x = x*(1.1*(nor_max_v-nor_min_v)/2)+(nor_max_v+nor_min_v)/2
+    #x = x*(1.1*(nor_max_v-nor_min_v)/2)+(nor_max_v+nor_min_v)/2
     '''
     This function is the constraints of potentianl flow, 
     L Phi = 0, L is the laplace calculator
@@ -220,24 +157,43 @@ def constraints(x, dx,dy, filtertf):
     # x.shape [batch_size, n_mesh, n_mesh, 2]
     u = tf.slice(x, [0,0,0,0], [batch_size, n_mesh, n_mesh, 1])
     v = tf.slice(x, [0,0,0,1], [batch_size, n_mesh, n_mesh, 1])
+    u = u*(1.1*(nor_max_v-nor_min_v)/2)+(nor_max_v+nor_min_v)/2
+    v = v*(1.1*(nor_max_v-nor_min_v)/2)+(nor_max_v+nor_min_v)/2
     
     u = tf.reshape(u,[batch_size, n_mesh, n_mesh])
     v = tf.reshape(v,[batch_size, n_mesh, n_mesh])
     
     u_left = tf.slice(u, [0,0,0], [batch_size, n_mesh, n_mesh-1])
     u_right = tf.slice(u, [0,0,1], [batch_size, n_mesh, n_mesh-1])
-    d_u = tf.divide(tf.subtract(u_right, u_left), dx)
-    
+      
     v_up = tf.slice(v, [0,0,0], [batch_size, n_mesh-1, n_mesh])
     v_down = tf.slice(v, [0,1,0], [batch_size, n_mesh-1, n_mesh])
-    d_v = tf.divide(tf.subtract(v_down, v_up), dy)
     
-    delta_u = tf.slice(d_u, [0,1,0],[batch_size, n_mesh-1, n_mesh-1])
-    delta_v = tf.slice(d_v, [0,0,1],[batch_size, n_mesh-1, n_mesh-1])
+    du = tf.subtract(u_right, u_left)
+    dv = tf.subtract(v_down, v_up)
+    
+    # partial 
+    du_dx = []
+    dv_dy = []
+    for i in range(batch_size):
+        du_dx_iter = tf.divide(du[i,:,:], dx)
+        du_dx.append(du_dx_iter)
+        
+        dv_dy_iter = tf.divide(dv[i,:,:], dy)
+        dv_dy.append(dv_dy_iter)
+    du_dx = tf.stack(du_dx)
+    dv_dy = tf.stack(dv_dy)
+    
+    delta_u = tf.slice(du_dx, [0,1,0], [batch_size, n_mesh-1, n_mesh-1])
+    delta_v = tf.slice(dv_dy, [0,0,1], [batch_size, n_mesh-1, n_mesh-1])
     
     divergence_field = delta_u+delta_v
     #filter divergence
-    divergence_filter = tf.multiply(divergence_field, filtertf)
+    divergence_filter = []
+    for i in range(batch_size):
+        divergence_filter.append(tf.multiply(divergence_field[i,:,:], filtertf))
+    divergence_filter = tf.stack(divergence_filter)
+    
     divergence_square = tf.square(divergence_filter)
     delta = tf.reduce_mean(divergence_square,2)
     divergence_mean = tf.reduce_mean(delta, 1)
@@ -256,9 +212,9 @@ x = tf.placeholder(tf.float32, shape=(None, n_mesh, n_mesh, n_label))
 z = tf.placeholder(tf.float32, shape=(None, 1, 1, 100))
 isTrain = tf.placeholder(dtype=tf.bool)
 
-dx = tf.placeholder(tf.float32, shape=(None, n_mesh, n_mesh-1))
-dy = tf.placeholder(tf.float32, shape=(None, n_mesh-1, n_mesh))
-filtertf = tf.placeholder(tf.float32, shape=(None, n_mesh-1, n_mesh-1))
+dx = tf.placeholder(tf.float32, shape=(n_mesh, n_mesh-1))
+dy = tf.placeholder(tf.float32, shape=(n_mesh-1, n_mesh))
+filtertf = tf.placeholder(tf.float32, shape=(n_mesh-1, n_mesh-1))
 
 with tf.variable_scope(tf.get_variable_scope()) as var_scope:
 
@@ -269,6 +225,7 @@ with tf.variable_scope(tf.get_variable_scope()) as var_scope:
     D_real, D_real_logits = discriminator(x, isTrain, reuse=tf.AUTO_REUSE)
     D_fake, D_fake_logits = discriminator(G_z, isTrain, reuse=tf.AUTO_REUSE)
     delta_lose, divergence_mean = constraints(G_z, dx, dy, filtertf)
+    print(delta_lose.shape)
     
     # trainable variables for each network
     T_vars = tf.trainable_variables()
@@ -303,9 +260,18 @@ with tf.variable_scope(tf.get_variable_scope()) as var_scope:
 
 init=tf.global_variables_initializer()
 
+
+filename_TFRecord = 'Potentialflow'+str(n_mesh)+'.tfrecord'
+# load tf.record
+queue_train = tf.data.TFRecordDataset(filename_TFRecord)
+dataset_train = queue_train.map(read_tfrecord).repeat().batch(batch_size)
+iterator_train = dataset_train.make_initializable_iterator()
+next_element_train = iterator_train.get_next()
+
 with tf.Session() as sess:
 
     sess.run(init)
+    sess.run(iterator_train.initializer)
     
     train_hist = {}
     train_hist['D_losses'] = []
@@ -330,21 +296,25 @@ with tf.Session() as sess:
         delta_real_record = []
         delta_lose_record = []
         epoch_start_time = time.time()
-        for iter in range(train_set.shape[0] // batch_size):
+        for iter in range(20000 // batch_size):
             # training discriminator
-            x_ = train_set[iter*batch_size:(iter+1)*batch_size]
+            train_set, _ = sess.run(next_element_train)
+            train_set[:,:,:,0:2] = (train_set[:,:,:,0:2]-(nor_max_v+nor_min_v)/2)/(1.1*(nor_max_v-nor_min_v)/2)
+            train_set[:,:,:,2] = (train_set[:,:,:,2]-(nor_max_p+nor_min_p)/2)/(1.1*(nor_max_p-nor_min_p)/2)
+            x_ = train_set
+            
             z_ = np.random.normal(0, 1, (batch_size, 1, 1, 100))
             
             loss_d_, _ = sess.run([D_loss, D_optim], {x: x_, z: z_, isTrain: True})
             
             # training generator
             z_ = np.random.normal(0, 1, (batch_size, 1, 1, 100))
-            loss_g_, _ = sess.run([G_loss, G_optim], {z:z_, x:x_, dx:d_x_, dy:d_y_, filtertf:filter_batch, isTrain: True})
+            loss_g_, _ = sess.run([G_loss, G_optim], {z:z_, x:x_, dx:d_x, dy:d_y, filtertf:filter, isTrain: True})
     
-            errD = D_loss.eval({z:z_, x:x_, filtertf:filter_batch, isTrain: False})
-            errG = G_loss_only.eval({z: z_, dx:d_x_, dy:d_y_, filtertf:filter_batch, isTrain: False})
-            errdelta_real = divergence_mean.eval({z:z_, dx:d_x_, dy:d_y_,filtertf:filter_batch, isTrain: False})
-            errdelta_lose = delta_lose.eval({z: z_, dx:d_x_, dy:d_y_,filtertf:filter_batch, isTrain: False})
+            errD = D_loss.eval({z:z_, x:x_, filtertf:filter, isTrain: False})
+            errG = G_loss_only.eval({z: z_, dx:d_x, dy:d_y, filtertf:filter, isTrain: False})
+            errdelta_real = divergence_mean.eval({z:z_, dx:d_x, dy:d_y,filtertf:filter, isTrain: False})
+            errdelta_lose = delta_lose.eval({z: z_, dx:d_x, dy:d_y,filtertf:filter, isTrain: False})
             
             D_losses.append(errD)
             G_losses.append(errG)
@@ -382,7 +352,7 @@ with tf.Session() as sess:
     
     end_time = time.time()
     total_ptime = end_time - start_time
-    name_data = root + 'PF-32-cons'+str(cons_value)+'-lam'+str(lam_cons)+'-lr'+str(lr_setting)+'-ep'+str(train_epoch)
+    name_data = root + 'Serial-results'+'-ep'+str(train_epoch)
     np.savez_compressed(name_data, a=train_hist, b=per_epoch_ptime)
     save_model = name_data+'.ckpt'
     save_path = saver.save(sess, save_model)
